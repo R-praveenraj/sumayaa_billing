4e37e6b019ea273db2754660b2b188c7f5190b27
R-praveenraj <praveenbarath123@gmail.com>
2024-08-03 01:10:04 +0530

    php sumayaa

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+include 'libs/load.php';
+?>
+
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="../assets/js/color-modes.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="sumayaa billing">
+    <meta name="author" content="praveenraj">
+    <meta name="generator" content="Hugo 0.122.0">
+    <title>Sumayaa Eye Plus</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/album/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+    <link href="/sumayaa/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+<header data-bs-theme="dark">
+  <?php load_template('_headers');
+  ?>
+</header>
+
+<main>
+  <?php load_template('_calltoaction'); ?>
+  <?php load_template('_billing'); ?>
+</main>
+
+<footer class="text-body-secondary py-5">
+<?php load_template('_footer'); ?>
+</footer>
+
+<script src="/sumayaa/assets/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
---
+++ libs/load.php
@@ -0,0 +1,5 @@
+<?php
+function load_template($name){
+    include $_SERVER['DOCUMENT_ROOT']."/SUMAYAA/_templates/$name.php";
+}
+?>
